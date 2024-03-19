aa3b72cd6a81b2d08974e9e303ffdc29b687adf1
KANA FUJIHIRA <kanapedrinisfujihira@gmail.com>
2024-03-19 10:56:38 +0100

    showing color

---
+++ index.js
@@ -0,0 +1,11 @@
+import chalk from 'chalk';
+
+const firstName = chalk.bold.blue;
+const secondName = chalk.bold.yellow;
+const thirdName = chalk.bold.red;
+const forthName = chalk.bold.green;
+
+console.log(firstName('Kana'));
+console.log(secondName('Bastien'));
+console.log(thirdName('Salma'));
+console.log(forthName('LeoPaul'));
